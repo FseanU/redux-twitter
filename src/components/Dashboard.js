93d06e626d80707aa2ebfcf1d85c79273b4784dc
Sean Fu <sean14xx@gmail.com>
2020-09-16 12:24:38 +0200

    feat: Render Tweet UI

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Tweet from './Tweet'
 
 class Dashboard extends React.Component {
   render () {
-    console.log(this.props)
     return (
       <div>
         <h3 className="center">Your Timeline</h3>
         <ul className="dashboard-list">
           {this.props.tweetIds.map((id) => (
             <li key={id}>
-              <div>TWEET ID: {id}</div>
+              <Tweet id={id} />
             </li>
           ))}
         </ul>
