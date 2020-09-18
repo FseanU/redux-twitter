6de8983777b5113971200d8f5d1dcfe9fccf18e3
Sean Fu <sean14xx@gmail.com>
2020-09-18 19:21:35 +0200

    feat: Add new Tweet UI

---
+++ src/components/NewTweet.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+class NewTweet extends React.Component {
+  state = {
+    text: '',
+  }
+
+  handleChange = (e) => {
+    const text = e.target.value
+
+    this.setState(() => ({
+      text
+    }))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { text } = this.state
+
+    // todo: Add tweet to store
+
+    console.log('New Tweet: ', text)
+
+    this.setState(() => ({
+      text: ''
+    }))
+  }
+
+  render() {
+    const { text } = this.state
+
+    {/* todo: Redirect to / if submitted */}
+    const tweetLeft = 280 - text.length
+
+    return(
+      <div>
+        <h3 className="center">Compose new Tweet</h3>
+        <form className="new-tweet" onSubmit={this.handleSubmit}>
+          <textarea 
+            placeholder="What is happening"
+            value={text}
+            onChange={this.handleChange}
+            className='textarea'
+            maxLength={280}
+          />
+          {tweetLeft <= 100 && (
+            <div className="tweet-length">
+              {tweetLeft}
+            </div>
+          )}
+          <button
+            className='btn'
+            type='submit'
+            disabled={text === ''}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default NewTweet
